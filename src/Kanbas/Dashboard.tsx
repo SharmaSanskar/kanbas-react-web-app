b3a70fab57af81af263807ca9997a857650b2f2e
Sanskar Sharma <sharmasanskar004@gmail.com>
2024-09-17 23:30:47 -0400

    Assignment editor HTML completed

---
+++ src/Kanbas/Dashboard.tsx
@@ -0,0 +1,141 @@
+import { Link } from "react-router-dom";
+
+function Dashboard() {
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <div id="wd-dashboard-courses">
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/reactjs.jpg" width={200} />
+            <div>
+              <h5>CS1234 React JS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/angular.jpg" width={200} />
+            <div>
+              <h5>CS1235 Angular JS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/vue.jpg" width={200} />
+            <div>
+              <h5>CS1236 Vue JS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/nextjs.jpg" width={200} />
+            <div>
+              <h5>CS1237 Next JS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/nodejs.jpg" width={200} />
+            <div>
+              <h5>CS1238 Node JS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/mongodb.jpg" width={200} />
+            <div>
+              <h5>CS1239 MongoDb</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/aws.jpg" width={200} />
+            <div>
+              <h5>CS1240 AWS</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+        <div className="wd-dashboard-course">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
+            <img alt="course" src="/images/azure.jpg" width={200} />
+            <div>
+              <h5>CS1241 Azure</h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/Labs/Lab2/index.tsx
@@ -0,0 +1,9 @@
+function Lab2() {
+  return (
+    <div id="wd-lab2">
+      <h2>Lab 2</h2>
+    </div>
+  );
+}
+
+export default Lab2;
