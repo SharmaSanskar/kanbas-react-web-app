f1eec1acc867cad37fba6580cfbe17099a32643d
Sanskar Sharma <sharmasanskar004@gmail.com>
2024-10-10 21:49:00 -0400

    Completed Labs 3 part

---
+++ src/Labs/Lab3/Square.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+function Square({ children }: { children: ReactNode }) {
+  const num = Number(children);
+  return <span id="wd-square">{num * num}</span>;
+}
+
+export default Square;
